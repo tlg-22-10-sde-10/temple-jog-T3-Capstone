2d64833839498094b0c3f08a40ec66286d69bcc4
Joseph Racke <joeracke@gmail.com>
2023-02-01 18:11:45 -0800

    Game setup, title, and manifest.mf for .jar, AB#78 AB#87

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        clearScreen();
         Scanner scanner = new Scanner(System.in);
+        System.out.println(TitleScreen.displaySetup());
+        scanner.nextLine();
+        clearScreen();
+        System.out.println(TitleScreen.displayTitle());
         System.out.println("Start Game? y/n");
         String playerInput = scanner.nextLine();
         playerInput = playerInput.toLowerCase().substring(0,1);
         if( playerInput.equals("y") ){
-            System.out.println(playerInput);
             Game game = new Game();
             System.out.println("game loads");
         }
         else System.out.println("Good Bye");
+        clearScreen();
+    }
+
+    private static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
 }

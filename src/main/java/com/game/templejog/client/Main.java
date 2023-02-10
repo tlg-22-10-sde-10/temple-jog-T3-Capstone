87717d1fb596046c73327afa3eacb5047853ef19
Joseph Racke <joeracke@gmail.com>
2023-02-10 15:32:06 -0800

    AB#738 fixed issue with ending scenarios

--- src/main/java/com/game/templejog/client/Main.java
+++ src/main/java/com/game/templejog/client/Main.java
@@ -68,7 +68,7 @@ public class Main {
                     && game.getPlayer().getSteps() < 24
                     && game.getPlayer().getHealth() > 0
                     && !(game.getCommunicatorOff() && game.getCurrentRoom().getName().equalsIgnoreCase("landing zone")));
-
+            console.displayResult("You look down as your alarm goes off. It's 18:00....",0);
             console.displayEnding();
         }
     }

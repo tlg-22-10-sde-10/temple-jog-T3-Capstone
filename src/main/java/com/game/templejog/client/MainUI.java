961843bbde25d0ef0c3d6eba7a27ffa69f8166e3
aguilkal <116393918+aguilkal@users.noreply.github.com>
2023-02-22 14:11:24 -0800

    fixed button colors on MAC AB#1317

--- src/main/java/com/game/templejog/client/MainUI.java
+++ src/main/java/com/game/templejog/client/MainUI.java
@@ -1,7 +1,14 @@
 package com.game.templejog.client;
 
+import javax.swing.*;
+
 public class MainUI {
     public static void main(String[] args) throws InterruptedException {
+        try{
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         new GameUI();
     }
 }
